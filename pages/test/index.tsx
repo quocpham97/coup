f3a420d1b97fc4cd57730d87453e3c951fb3fb4b
quocpham97 <phamxuandinhquoc@gmail.com>
2022-12-20 15:39:22 +0700

    init tailwind config, eslint config

---
+++ pages/test/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TestPage() {
+  return <div className="text-red-500">TestPage</div>;
+}
+
+export default TestPage;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}',"./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
