f114ef226c6fa60c659d6474150cc90944c12ddb
quocpham97 <phamxuandinhquoc@gmail.com>
2023-02-02 10:53:34 +0700

    sign in with github and google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
 import ConfigureAbly from 'components/ConfigureAbly';
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: Session }>) {
   return (
-    <ConfigureAbly>
-      <Component {...pageProps} />
-    </ConfigureAbly>
+    <SessionProvider session={session}>
+      <ConfigureAbly>
+        <Component {...pageProps} />
+      </ConfigureAbly>
+    </SessionProvider>
   );
 }
 
