114a1da91a5f940cf0d966a8f655600dbfbbb686
quocpham97 <phamxuandinhquoc@gmail.com>
2023-02-21 16:40:38 +0700

    fix bug

--- src/components/test/index.tsx
+++ src/components/test/index.tsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { useAction } from 'hooks/useAction';
 import { Character } from 'types';
-import Action from 'components/Action';
 // import { generateCards } from 'utils/gameGenerator';
 
 function TestComponent() {
   const [cards, setCards] = useState<Character[]>([]);
-  const { actionTypes } = useAction();
 
   useEffect(() => {
     // setCards(generateCards());
@@ -19,11 +16,6 @@ function TestComponent() {
       {cards.map((c, index) => (
         <div key={`${c}-${index + 1}`}>{c}</div>
       ))}
-      <div className="flex gap-2">
-        {actionTypes.map((a, index) => (
-          <Action key={`${a}-${index + 1}`} type={a} isDisabled={index % 2 === 0} />
-        ))}
-      </div>
     </div>
   );
 }
